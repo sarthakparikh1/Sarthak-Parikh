850d751f287fe6b34c80f865a891c2bdc5e12105
SarthakParikh <sarthakparikh20010409@gmail.com>
2022-09-08 20:51:28 +0530

    Created React Native Application

---
+++ React-Native/Application_Login/App.js
@@ -0,0 +1,19 @@
+import { View, Text, StyleSheet } from 'react-native'
+import React from 'react'
+
+const App = () => {
+  return (
+    <View>
+      <Text>App</Text>
+    </View>
+  )
+}
+
+export default App
+
+const Style = StyleSheet.create({
+  rootScreen:{
+    flex:1,
+    backgroundColor: "#5fff",
+  }
+}) 
